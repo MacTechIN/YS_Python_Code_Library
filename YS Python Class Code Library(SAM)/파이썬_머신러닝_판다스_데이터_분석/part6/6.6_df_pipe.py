fd1db82612adad4a9b379e3e88759c9735821fe9
Sam LEE <samlee@cufs.ac.kr>
2023-09-08 09:58:44 +0900

    Update 6.6_df_pipe.py

--- YS Python Class Code Library(SAM)/파이썬_머신러닝_판다스_데이터_분석/part6/6.6_df_pipe.py
+++ YS Python Class Code Library(SAM)/파이썬_머신러닝_판다스_데이터_분석/part6/6.6_df_pipe.py
@@ -15,9 +15,12 @@ def missing_value(x):
 def missing_count(x):    # 
     return missing_value(x).sum()
 
+
+
 # 데이터프레임의 총 NaN 개수 - 데이터프레임 전달하면 값을 반환
 def totoal_number_missing(x):    
     return missing_count(x).sum()
+
     
 # 데이터프레임에 pipe() 메소드로 함수 매핑
 result_df = df.pipe(missing_value)   
@@ -29,6 +32,7 @@ result_series = df.pipe(missing_count)
 print(result_series)
 print(type(result_series))
 print('\n')
+
 
 result_value = df.pipe(totoal_number_missing)   
 print(result_value)
